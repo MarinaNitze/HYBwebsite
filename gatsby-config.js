d52c1a409f8abd1ed962551fa4f4bed97702a080
Marina Nitze <marinanitze@marinas.macbook.air.lan>
2022-05-17 09:34:10 -0700

    Adding Google Analytics via gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,33 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
 	`gatsby-plugin-netlify`,
+	{
+	      resolve: `gatsby-plugin-google-gtag`,
+	      options: {
+	        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+	        trackingIds: [
+	          'G-LRF971PJMB', // Google Analytics / GA
+	          // optional
+	          //'OPTIONAL----AW-CONVERSION_ID', // Google Ads / Adwords / AW
+	          //'OPTIONAL----DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+	        ],
+	        // This object gets passed directly to the gtag config command
+	        // This config will be shared across all trackingIds
+	        //gtagConfig: {
+	        //  optimize_id: 'OPT_CONTAINER_ID',
+	        //  anonymize_ip: true,
+	        //  cookie_expires: 0,
+	        //},
+	        // This object is used for configuration specific to this plugin
+	        pluginConfig: {
+	          // Puts tracking script in the head instead of the body
+	          head: false,
+	          // Setting this parameter is also optional
+	          respectDNT: true,
+	          // Avoids sending pageview hits from custom paths
+	          exclude: ['/preview/**', '/do-not-track/me/too/'],
+	        },
+	      },
+	    },
   ],
 };
