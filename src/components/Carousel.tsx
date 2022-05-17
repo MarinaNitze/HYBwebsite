06d1c7bc297e4f8884b36e409dfa8bae8f9e1faa
Marina Nitze <marinanitze@marinas.macbook.air.lan>
2022-05-17 09:48:37 -0700

    Added Eric blurb

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -17,6 +17,17 @@ export default function Carousel() {
       <div className="carousel-cell">
         <div className="quote">
           <div className="quote-wrapper">
+            <div className="quote-name">Eric Schmidt</div>
+            <div className="quote-position">co-founder of Schmidt Futures and former CEO of Google</div>
+            <div className="quote-content">
+              "A master class on intrapreneurship.  If you want to drive change in large organizations, <em>Hack Your Bureaucracy</em> is a must read."
+            </div>
+          </div>
+        </div>
+      </div>
+	  <div className="carousel-cell">
+        <div className="quote">
+          <div className="quote-wrapper">
             <div className="quote-name">Cecilia Muñoz</div>
             <div className="quote-position">former Director, White House Domestic Policy Council under President Obama</div>
             <div className="quote-content">
@@ -28,10 +39,10 @@ export default function Carousel() {
       <div className="carousel-cell">
         <div className="quote">
           <div className="quote-wrapper">
-            <div className="quote-name">Carl Malamud</div>
-            <div className="quote-position">Public Resource</div>
+            <div className="quote-name">Todd Park</div>
+            <div className="quote-position">cofounder of Devoted Health, Athenahealth, and Castlight Health and former U.S. CTO</div>
             <div className="quote-content">
-              "The U.S. federal government is the largest and most unwieldy living organism in the world. In <em>Hack Your Bureaucracy</em>, Marina Nitze and Nick Sinai use their own experience as agents of change in the civil service to give a master class on how to get things done. This book is essential reading for anybody working in any large institution, not just government."
+              "Nick and Marina are incredibly gifted change agents, and in <em>Hack Your Bureaucracy</em>, they provide hard-won lessons and wisdom that will be invaluable to everyone from entrepreneurs trying to build great companies to innovators working to change institutions of all kinds from the inside out.  Like working with Nick and Marina, the book is energizing, inspiring, and an absolute blast -- a how-to manual for driving change unlike any other."
             </div>
           </div>
         </div>
@@ -39,10 +50,10 @@ export default function Carousel() {
       <div className="carousel-cell">
         <div className="quote">
           <div className="quote-wrapper">
-            <div className="quote-name">Todd Park</div>
-            <div className="quote-position">cofounder of Devoted Health, Athenahealth, and Castlight Health and former U.S. CTO</div>
+            <div className="quote-name">Carl Malamud</div>
+            <div className="quote-position">Public Resource</div>
             <div className="quote-content">
-              "Nick and Marina are incredibly gifted change agents, and in <em>Hack Your Bureaucracy</em>, they provide hard-won lessons and wisdom that will be invaluable to everyone from entrepreneurs trying to build great companies to innovators working to change institutions of all kinds from the inside out.  Like working with Nick and Marina, the book is energizing, inspiring, and an absolute blast -- a how-to manual for driving change unlike any other."
+              "The U.S. federal government is the largest and most unwieldy living organism in the world. In <em>Hack Your Bureaucracy</em>, Marina Nitze and Nick Sinai use their own experience as agents of change in the civil service to give a master class on how to get things done. This book is essential reading for anybody working in any large institution, not just government."
             </div>
           </div>
         </div>
