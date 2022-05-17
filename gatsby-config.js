c812f8ca7560399326011088f8d2a27197bb9c18
Marina Nitze <marinanitze@marinas.macbook.air.lan>
2022-05-17 09:43:02 -0700

    Fiddling with Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,9 +50,9 @@ module.exports = {
 	        // This object is used for configuration specific to this plugin
 	        pluginConfig: {
 	          // Puts tracking script in the head instead of the body
-	          head: false,
+	          head: true,
 	          // Setting this parameter is also optional
-	          respectDNT: true,
+	          //respectDNT: true,
 	          // Avoids sending pageview hits from custom paths
 	          exclude: ['/preview/**', '/do-not-track/me/too/'],
 	        },
