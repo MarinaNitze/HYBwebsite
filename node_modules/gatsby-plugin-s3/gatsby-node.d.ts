4e7a6123a9803e0664d6cde506dc2b5d8522239a
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2022-07-20 13:43:40 -0400

    Remove node_modules and cache folders

--- node_modules/gatsby-plugin-s3/gatsby-node.d.ts
+++
@@ -1,4 +0,0 @@
-import { GatsbyNode } from 'gatsby';
-export declare const onPreBootstrap: GatsbyNode['createPagesStatefully'];
-export declare const createPagesStatefully: GatsbyNode['createPagesStatefully'];
-export declare const onPostBuild: GatsbyNode['onPostBuild'];
