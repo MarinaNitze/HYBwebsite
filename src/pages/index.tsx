03b4394c91e8e2e4038bee6331f375edbfcb4584
Marina Nitze <marinanitze@marinas.macbook.air.lan>
2022-09-04 17:16:23 -0700

    Retrying FB code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,24 +45,24 @@ export default function Home() {
           }
         }`}
       </script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '562225513945244');
-      fbq('track', 'PageView'); `,
-        }}
-      />
-      <noscript>
-        {`<img height="1" width="1" style={"display:none"} src="https://www.facebook.com/tr?id=562225513945244&ev=PageView&noscript=1" />`}
-      </noscript>
+	  <script
+	            dangerouslySetInnerHTML={{
+	              __html: `
+	          !function(f,b,e,v,n,t,s)
+	          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	          n.queue=[];t=b.createElement(e);t.async=!0;
+	          t.src=v;s=b.getElementsByTagName(e)[0];
+	          s.parentNode.insertBefore(t,s)}(window, document,'script',
+	          'https://connect.facebook.net/en_US/fbevents.js');
+	          fbq('init', '562225513945244');
+	          fbq('track', 'PageView'); `,
+	            }}
+	          />
+	          <noscript>
+	            {`<img height="1" width="1" style={"display:none"} src="https://www.facebook.com/tr?id=562225513945244&ev=PageView&noscript=1" />`}
+	          </noscript>
     </Helmet>
       <div className="wrapper">
         <header className="hero">
